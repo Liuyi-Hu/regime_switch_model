526bd4959549e838fdb223c715f868858cb9d7ce
Lori Hu <lhu@ncsu.edu>
2017-11-08 22:18:14 -0500

    add tutorial

--- regime_switch_model/__init__.py
+++ regime_switch_model/__init__.py
@@ -4,4 +4,4 @@ regime_switch_model
 ``regime_switch_model`` is a set of algorithms for learning and inference of
 Regime-Switching Models widely used in finance and economics.
 """
-
+from .rshmm import *
